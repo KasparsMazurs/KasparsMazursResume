ea98f2d21b7fbf4d477f8ae3682b71fc4b297d2a
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-08-10 13:41:01 +0000

    fix the isue with emails

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,18 @@
 function sendMail(contactForm) {
     emailjs.send('kaspars.mazurs@gmail.com', 'template_tlod5re', {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                contactForm.reset(); // Reset the form fields
+                window.location.href = '/thank_you.html'; // Redirect to the thank you pag
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
